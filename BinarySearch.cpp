8f767d986109e4f3d9906febc45f5829ad7c5f78
yudhistirageraldo <yudhistirageraldo@gmail.com>
2026-04-16 10:45:49 +0700

    array setelah di urutkan

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -35,4 +35,28 @@ void input()
 
 void bubbleSortArray()
 {
-    
+    int pass = 1;
+    do 
+    {
+        for (int j = 0; j < nPanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+        pass++;
+    } while (pass <= nPanjang - 1);
+}
+
+void display()
+{
+    cout << "\n============================================\n";
+    cout << " Element Array Setelah Diurutkan (Asc) \n";
+    cout << "==============================================\n";
+
+    for (int j = 0; j < nPanjang; j++)
+    {
+        
